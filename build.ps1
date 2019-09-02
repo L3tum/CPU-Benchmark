deb9dd65d37244aa774ab02d587583762b9fbf1d
L3tum <9307432+L3tum@users.noreply.github.com>
2019-09-02 13:08:20 +0200

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -34,3 +34,16 @@ Copy-Item ../LICENSE bin/Release/Benchmarker-linux64/LICENSE
 Copy-Item ../LICENSE bin/Release/Benchmarker-debian64/LICENSE
 Copy-Item ../LICENSE bin/Release/Benchmarker-ubuntu64/LICENSE
 Copy-Item ../LICENSE bin/Release/Benchmarker-osx64/LICENSE
+
+mkdir bin/Release/ZIPs
+
+Compress-Archive -Path bin/Release/Benchmarker -DestinationPath bin/Release/ZIPs/Benchmarker.zip
+Compress-Archive -Path bin/Release/Benchmarker-win64 -DestinationPath bin/Release/ZIPs/Benchmarker-win64.zip
+Compress-Archive -Path bin/Release/Benchmarker-win32 -DestinationPath bin/Release/ZIPs/Benchmarker-win32.zip
+Compress-Archive -Path bin/Release/Benchmarker-linux-arm32v7 -DestinationPath bin/Release/ZIPs/Benchmarker-linux-arm32v7.zip
+Compress-Archive -Path bin/Release/Benchmarker-win-arm32v7 -DestinationPath bin/Release/ZIPs/Benchmarker-win-arm32v7.zip
+Compress-Archive -Path bin/Release/Benchmarker-win-arm64 -DestinationPath bin/Release/ZIPs/Benchmarker-win-arm64.zip
+Compress-Archive -Path bin/Release/Benchmarker-linux64 -DestinationPath bin/Release/ZIPs/Benchmarker-linux64.zip
+Compress-Archive -Path bin/Release/Benchmarker-debian64 -DestinationPath bin/Release/ZIPs/Benchmarker-debian64.zip
+Compress-Archive -Path bin/Release/Benchmarker-ubuntu64 -DestinationPath bin/Release/ZIPs/Benchmarker-ubuntu64.zip
+Compress-Archive -Path bin/Release/Benchmarker-osx64 -DestinationPath bin/Release/ZIPs/Benchmarker-osx64.zip
