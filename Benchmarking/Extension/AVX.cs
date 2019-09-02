5fd15be31d9d76f86ed7e077c1dc17a208bf7df4
L3tum <9307432+L3tum@users.noreply.github.com>
2019-09-02 23:00:44 +0200

    Add multiplication to avx to better represent common workloads

--- Benchmarking/Extension/AVX.cs
+++ Benchmarking/Extension/AVX.cs
@@ -53,6 +53,7 @@ namespace Benchmarking.Extension
 					for (var j = 0; j < iterations; j++)
 					{
 						AddScalarU(randomFloatingSpan, dst);
+						MultiplyScalarU(randomFloatingSpan, dst);
 					}
 				});
 			}
@@ -63,7 +64,7 @@ namespace Benchmarking.Extension
 
 		public override string GetDescription()
 		{
-			return "AVX benchmark by adding two vectors of 256 bit (4 floats) from a big vector of 1024 bit";
+			return "AVX benchmark with big vectors";
 		}
 
 		public override void Initialize()
@@ -83,14 +84,14 @@ namespace Benchmarking.Extension
 		{
 			if (options.Threads == 1)
 			{
-				return 51493.0d;
+				return 164184.0d;
 			}
 
-			return 10691.0d;
+			return 24795.0d;
 		}
 
 #if NETCOREAPP3_0
-		private unsafe void AssignScalarU(Span<float> scalar, Span<float> dst)
+		private unsafe void MultiplyScalarU(Span<float> scalar, Span<float> dst)
 		{
 			fixed (float* pdst = dst)
 			fixed (float* psrc = scalar)
@@ -102,7 +103,9 @@ namespace Benchmarking.Extension
 
 				while (pDstCurrent + 8 <= pDstEnd)
 				{
-					Avx.Store(pDstCurrent, scalarVector256);
+					var dstVector = Avx.LoadVector256(pDstCurrent);
+					dstVector = Avx.Multiply(dstVector, scalarVector256);
+					Avx.Store(pDstCurrent, dstVector);
 
 					pDstCurrent += 8;
 				}
@@ -111,14 +114,18 @@ namespace Benchmarking.Extension
 
 				if (pDstCurrent + 4 <= pDstEnd)
 				{
-					Sse.Store(pDstCurrent, scalarVector128);
+					var dstVector = Sse.LoadVector128(pDstCurrent);
+					dstVector = Sse.Multiply(dstVector, scalarVector128);
+					Sse.Store(pDstCurrent, dstVector);
 
 					pDstCurrent += 4;
 				}
 
 				while (pDstCurrent < pDstEnd)
 				{
-					Sse.StoreScalar(pDstCurrent, scalarVector128);
+					var dstVector = Sse.LoadScalarVector128(pDstCurrent);
+					dstVector = Sse.MultiplyScalar(dstVector, scalarVector128);
+					Sse.StoreScalar(pDstCurrent, dstVector);
 
 					pDstCurrent++;
 				}
