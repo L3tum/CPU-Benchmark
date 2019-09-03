f99cece0cd153ec586e2a1a4cf55c61375fd3b7f
L3tum <9307432+L3tum@users.noreply.github.com>
2019-09-03 23:12:51 +0200

    Feature/add changelog generator (#46)
    * Add changelog (generator)
    
    * Updated changelog generator and template

---
+++ generate_changelog.ps1
@@ -0,0 +1,11 @@
+$future = $args[0]
+
+if($future -eq $null -or $future -eq ""){
+    github_changelog_generator -u L3tum -p CPU-Benchmark --cache-log ./tmp --token $(Get-Content "./github_token") --no-pr-wo-labels --no-issues --pr-label "**Updates:**"
+} else{
+    github_changelog_generator -u L3tum -p CPU-Benchmark --cache-log ./tmp --future-release $future --token $(Get-Content "./github_token") --no-pr-wo-labels --no-issues --pr-label "**Updates:**"
+}
+Remove-Item "./tmp"
+
+Write-Host "Press any key to continue..."
+$Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
