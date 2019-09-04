fdef95ba7912a5328dcf41094c20f8af41160067
L3tum <9307432+L3tum@users.noreply.github.com>
2019-09-04 23:01:53 +0200

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,19 +1,20 @@
-$version = "0.1.1"
+$version="0.2.0"
 
 cd Benchmarker
 Remove-Item -Path bin/Release -Recurse
 mkdir bin/Release
 dotnet restore Benchmarker.csproj
-dotnet publish Benchmarker.csproj /p:AssemblyVersion=$version --framework netcoreapp3.0 -c Release -o bin/Release/Benchmarker
-dotnet publish Benchmarker.csproj /p:AssemblyVersion=$version --framework netcoreapp3.0 -c Release -r win-x64 /p:PublishSingleFile=true /p:PublishTrimmed=true -o bin/Release/Benchmarker-win64
-dotnet publish Benchmarker.csproj /p:AssemblyVersion=$version --framework netcoreapp3.0 -c Release -r win-x86 /p:PublishSingleFile=true /p:PublishTrimmed=true -o bin/Release/Benchmarker-win32
-dotnet publish Benchmarker.csproj /p:AssemblyVersion=$version --framework netcoreapp3.0 -c Release -r linux-arm /p:PublishSingleFile=true /p:PublishTrimmed=true -o bin/Release/Benchmarker-linux-arm32v7
-dotnet publish Benchmarker.csproj /p:AssemblyVersion=$version --framework netcoreapp3.0 -c Release -r win-arm /p:PublishSingleFile=true /p:PublishTrimmed=true -o bin/Release/Benchmarker-win-arm32v7
-dotnet publish Benchmarker.csproj /p:AssemblyVersion=$version --framework netcoreapp3.0 -c Release -r win-arm64 /p:PublishSingleFile=true /p:PublishTrimmed=true -o bin/Release/Benchmarker-win-arm64
-dotnet publish Benchmarker.csproj /p:AssemblyVersion=$version --framework netcoreapp3.0 -c Release -r linux-x64 /p:PublishSingleFile=true /p:PublishTrimmed=true -o bin/Release/Benchmarker-linux64
-dotnet publish Benchmarker.csproj /p:AssemblyVersion=$version --framework netcoreapp3.0 -c Release -r debian-x64 /p:PublishSingleFile=true /p:PublishTrimmed=true -o bin/Release/Benchmarker-debian64
-dotnet publish Benchmarker.csproj /p:AssemblyVersion=$version --framework netcoreapp3.0 -c Release -r ubuntu-x64 /p:PublishSingleFile=true /p:PublishTrimmed=true -o bin/Release/Benchmarker-ubuntu64
-dotnet publish Benchmarker.csproj /p:AssemblyVersion=$version --framework netcoreapp3.0 -c Release -r osx-x64 /p:PublishSingleFile=true /p:PublishTrimmed=true -o bin/Release/Benchmarker-osx64
+
+dotnet publish Benchmarker.csproj --framework netcoreapp3.0 -c Release -o bin/Release/Benchmarker /p:VersionSuffix=$version
+dotnet publish Benchmarker.csproj --framework netcoreapp3.0 -c Release -r win-x64 /p:PublishSingleFile=true /p:PublishTrimmed=true -o bin/Release/Benchmarker-win64 /p:VersionSuffix=$version
+dotnet publish Benchmarker.csproj --framework netcoreapp3.0 -c Release -r win-x86 /p:PublishSingleFile=true /p:PublishTrimmed=true -o bin/Release/Benchmarker-win32 /p:VersionSuffix=$version
+dotnet publish Benchmarker.csproj --framework netcoreapp3.0 -c Release -r linux-arm /p:PublishSingleFile=true /p:PublishTrimmed=true -o bin/Release/Benchmarker-linux-arm32v7 /p:VersionSuffix=$version
+dotnet publish Benchmarker.csproj --framework netcoreapp3.0 -c Release -r win-arm /p:PublishSingleFile=true /p:PublishTrimmed=true -o bin/Release/Benchmarker-win-arm32v7 /p:VersionSuffix=$version
+dotnet publish Benchmarker.csproj --framework netcoreapp3.0 -c Release -r win-arm64 /p:PublishSingleFile=true /p:PublishTrimmed=true -o bin/Release/Benchmarker-win-arm64 /p:VersionSuffix=$version
+dotnet publish Benchmarker.csproj --framework netcoreapp3.0 -c Release -r linux-x64 /p:PublishSingleFile=true /p:PublishTrimmed=true -o bin/Release/Benchmarker-linux64 /p:VersionSuffix=$version
+dotnet publish Benchmarker.csproj --framework netcoreapp3.0 -c Release -r debian-x64 /p:PublishSingleFile=true /p:PublishTrimmed=true -o bin/Release/Benchmarker-debian64 /p:VersionSuffix=$version
+dotnet publish Benchmarker.csproj --framework netcoreapp3.0 -c Release -r ubuntu-x64 /p:PublishSingleFile=true /p:PublishTrimmed=true -o bin/Release/Benchmarker-ubuntu64 /p:VersionSuffix=$version
+dotnet publish Benchmarker.csproj --framework netcoreapp3.0 -c Release -r osx-x64 /p:PublishSingleFile=true /p:PublishTrimmed=true -o bin/Release/Benchmarker-osx64 /p:VersionSuffix=$version
 
 Copy-Item ../README.md bin/Release/Benchmarker/README.md
 Copy-Item ../README.md bin/Release/Benchmarker-win64/README.md
