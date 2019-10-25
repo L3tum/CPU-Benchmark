df998ceb4b4679f1d9116090b5b48daed014f387
L3tum <9307432+L3tum@users.noreply.github.com>
2019-10-25 22:38:35 +0200

    Add decompression benchmarks

---
+++ Benchmarking/Decompression/GZip.cs
@@ -0,0 +1,108 @@
+﻿#region using
+
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Benchmarking.Util;
+using ICSharpCode.SharpZipLib.GZip;
+
+#endregion
+
+namespace Benchmarking.Decompression
+{
+	internal class GZip : Benchmark
+	{
+		private readonly byte[][] datas;
+
+		public GZip(Options options) : base(options)
+		{
+			datas = new byte[options.Threads][];
+		}
+
+		public override void Run()
+		{
+			var tasks = new Task[options.Threads];
+
+			for (var i = 0; i < options.Threads; i++)
+			{
+				var i1 = i;
+				tasks[i] = Task.Run(() =>
+				{
+					using (Stream s = new MemoryStream(datas[i1]))
+					{
+						using var stream = new GZipInputStream(s);
+						using var sr = new StreamReader(stream);
+						sr.ReadToEnd();
+					}
+
+					BenchmarkRunner.ReportProgress(GetName());
+				});
+			}
+
+			Task.WaitAll(tasks);
+		}
+
+		public override string GetName()
+		{
+			return base.GetName() + "-decompression";
+		}
+
+		public override string GetDescription()
+		{
+			return "Decompressing 1 GB of data with GZip";
+		}
+
+		public override void Initialize()
+		{
+			var tasks = new Task[options.Threads];
+
+			// 500 "MB" string -> 2 bytes per character -> 1 GB String
+			for (var i = 0; i < options.Threads; i++)
+			{
+				var i1 = i;
+
+				tasks[i1] = Task.Run(() =>
+				{
+					// 500000000
+					var data = DataGenerator.GenerateString((int) (500000000 / options.Threads));
+
+					using (var s = new MemoryStream())
+					{
+						using (var stream = new GZipOutputStream(s))
+						{
+							stream.SetLevel(9);
+
+							using var sw = new StreamWriter(stream);
+							sw.Write(data);
+							sw.Flush();
+							stream.Finish();
+
+							stream.IsStreamOwner = false;
+						}
+
+						s.Seek(0, SeekOrigin.Begin);
+
+						datas[i1] = s.ToArray();
+					}
+				});
+			}
+
+			Task.WaitAll(tasks);
+		}
+
+		public override double GetReferenceValue()
+		{
+			if (options.Threads == 1)
+			{
+				return 7279.0d;
+			}
+
+			return 3598.0d;
+		}
+
+		public override string GetCategory()
+		{
+			return "decompression";
+		}
+	}
+}
---
+++ Benchmarking/Decompression/ZIP.cs
@@ -0,0 +1,123 @@
+﻿#region using
+
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Benchmarking.Util;
+using ICSharpCode.SharpZipLib.Zip;
+
+#endregion
+
+namespace Benchmarking.Decompression
+{
+	public class ZIP : Benchmark
+	{
+		private readonly string[] datas;
+
+		public ZIP(Options options) : base(options)
+		{
+			datas = new string[options.Threads];
+		}
+
+		public override void Run()
+		{
+			var tasks = new Task[options.Threads];
+
+			for (var i = 0; i < options.Threads; i++)
+			{
+				var i1 = i;
+				tasks[i] = Task.Run(() =>
+				{
+					using (Stream s = new MemoryStream())
+					{
+						using (var sw = new StreamWriter(s))
+						{
+
+							sw.Write(datas[i1]);
+							sw.Flush();
+
+							s.Seek(0, SeekOrigin.Begin);
+
+							using var stream = new ZipInputStream(s);
+							datas[i1] = stream.GetNextEntry().ToString();
+							stream.IsStreamOwner = false;
+						}
+					}
+
+					BenchmarkRunner.ReportProgress(GetName());
+				});
+			}
+
+			Task.WaitAll(tasks);
+		}
+
+		public override string GetName()
+		{
+			return base.GetName() + "-decompression";
+		}
+
+		public override string GetDescription()
+		{
+			return "Decompressing 1 GB of data with ZIP";
+		}
+
+		public override void Initialize()
+		{
+			var tasks = new Task[options.Threads];
+
+			// 500 "MB" string -> 2 bytes per character -> 1 GB String
+			for (var i = 0; i < options.Threads; i++)
+			{
+				var i1 = i;
+
+				tasks[i1] = Task.Run(() =>
+				{
+					// 500000000
+					var data = DataGenerator.GenerateString((int) (500000000 / options.Threads));
+
+					using (Stream s = new MemoryStream())
+					{
+						using (var stream = new ZipOutputStream(s))
+						{
+
+							stream.SetLevel(9);
+
+							var entry = new ZipEntry("test.txt") {DateTime = DateTime.Now};
+
+							stream.PutNextEntry(entry);
+
+							using var sw = new StreamWriter(stream);
+							sw.Write(data);
+							sw.Flush();
+
+							stream.CloseEntry();
+							stream.IsStreamOwner = false;
+						}
+
+						s.Seek(0, SeekOrigin.Begin);
+
+						using var sr = new StreamReader(s);
+						datas[i1] = sr.ReadToEnd();
+					}
+				});
+			}
+
+			Task.WaitAll(tasks);
+		}
+
+		public override double GetReferenceValue()
+		{
+			if (options.Threads == 1)
+			{
+				return 1864.0d;
+			}
+
+			return 256.0d;
+		}
+
+		public override string GetCategory()
+		{
+			return "decompression";
+		}
+	}
+}
